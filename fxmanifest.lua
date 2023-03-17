2603e88ca004224246126be264ed3e60add38b49
25danijelmesec03 <128039784+25danijelmesec03@users.noreply.github.com>
2023-03-17 11:50:21 +0100

    Needed to start the car

---
+++ fxmanifest.lua
@@ -0,0 +1,146 @@
+fx_version 'cerulean'
+lua54 'on'
+games { 'gta5' }
+
+description 'FiveM Automobili'
+version '0.0.1'
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+	'/ddscript_zvkovi/audioconfig/m5cracklemod_game.dat151.rel',
+	'/ddscript_zvkovi/audioconfig/m5cracklemod_sounds.dat54.rel',
+	'/ddscript_zvkovi/sfx/dlc_m5cracklemod/m5cracklemod.awc',
+	'/ddscript_zvkovi/sfx/dlc_m5cracklemod/m5cracklemod_npc.awc',
+    '/ddscript_zvkovi/audioconfig/aston59v12_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/aston59v12_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_aston59v12/aston59v12.awc',
+    '/ddscript_zvkovi/sfx/dlc_aston59v12/aston59v12_npc.awc',
+    '/ddscript_zvkovi/audioconfig/lambov10_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/lambov10_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_lambov10/lambov10.awc',
+    '/ddscript_zvkovi/sfx/dlc_lambov10/lambov10_npc.awc',
+    '/ddscript_zvkovi/audioconfig/*.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/*.dat54.rel',
+    '/ddscript_zvkovi/sfx/**/*.awc',
+    '/ddscript_zvkovi/audioconfig/k20a_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/k20a_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_k20a/k20a.awc',
+    '/ddscript_zvkovi/sfx/dlc_k20a/k20a_npc.awc',
+    '/ddscript_zvkovi/audioconfig/k20c_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/k20c_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_k20c/k20c.awc',
+    '/ddscript_zvkovi/sfx/dlc_k20c/k20c_npc.awc',
+    '/ddscript_zvkovi/audioconfig/wrxrllyeng_amp.dat10.rel',
+	'/ddscript_zvkovi/audioconfig/wrxrllyeng_game.dat151.rel',
+	'/ddscript_zvkovi/audioconfig/wrxrllyeng_sounds.dat54.rel',
+	'/ddscript_zvkovi/sfx/dlc_wrxrllyeng/wrxrllyeng.awc',
+	'/ddscript_zvkovi/sfx/dlc_wrxrllyeng/wrxrllyeng_npc.awc',
+    "/ddscript_zvkovi/audioconfig/*.dat151.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat54.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat10.rel",
+    '/ddscript_zvkovi/audioconfig/chevroletlt4_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/chevroletlt4_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_chevroletlt4/chevroletlt4.awc',
+    '/ddscript_zvkovi/sfx/dlc_chevroletlt4/chevroletlt4_npc.awc',
+    '/ddscript_zvkovi/audioconfig/*.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/*.dat54.rel',
+    "/ddscript_zvkovi/audioconfig/*.dat151.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat54.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat10.rel",
+    "/ddscript_zvkovi/audioconfig/*.dat151.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat54.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat10.rel",
+    '/ddscript_zvkovi/audioconfig/ea825_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/ea825_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_ea825/ea825.awc',
+    '/ddscript_zvkovi/sfx/dlc_ea825/ea825_npc.awc',
+    '/ddscript_zvkovi/audioconfig/mercm177_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/mercm177_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_mercm177/mercm177.awc',
+    '/ddscript_zvkovi/sfx/dlc_mercm177/mercm177_npc.awc',
+    '/ddscript_zvkovi/audioconfig/tagt3flat6_game.dat151.rel',
+    '/ddscript_zvkovi/audioconfig/tagt3flat6_sounds.dat54.rel',
+    '/ddscript_zvkovi/sfx/dlc_tagt3flat6/tagt3flat6.awc',
+    '/ddscript_zvkovi/sfx/dlc_tagt3flat6/tagt3flat6_npc.awc',
+    "/ddscript_zvkovi/audioconfig/*.dat151.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat54.rel",
+	"/ddscript_zvkovi/audioconfig/*.dat10.rel",
+    "/ddscript_zvkovi/audioconfig/ea888_game.dat151.rel",
+    "/ddscript_zvkovi/audioconfig/ea888_sounds.dat54.rel",
+    "/ddscript_zvkovi/sfx/dlc_ea888/ea888.awc",
+    "/ddscript_zvkovi/sfx/dlc_ea888/ea888_npc.awc",
+}
+
+data_file 'HANDLING_FILE' '/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '/**/vehicles.meta'
+data_file 'CARCOLS_FILE' '/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '/**/carvariations.meta'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/m5cracklemod_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/m5cracklemod_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_m5cracklemod'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/aston59v12_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/aston59v12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_aston59v12'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/lambov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_lambov10'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/porsche57v10_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/porsche57v10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_porsche57v10'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/k20a_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/k20a_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_k20a'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/k20c_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/k20c_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_k20c'
+data_file 'AUDIO_SYNTHDATA' '/ddscript_zvkovi/audioconfig/wrxrllyeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/wrxrllyeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/wrxrllyeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_wrxrllyeng'
+data_file "AUDIO_SYNTHDATA" "/ddscript_zvkovi/audioconfig/nsr2teng_amp.dat"
+data_file "AUDIO_GAMEDATA" "/ddscript_zvkovi/audioconfig/nsr2teng_game.dat"
+data_file "AUDIO_SOUNDDATA" "/ddscript_zvkovi/audioconfig/nsr2teng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "/ddscript_zvkovi/sfx/dlc_nsr2teng"
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/chevroletlt4_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/chevroletlt4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_chevroletlt4'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/tamustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/tamustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_tamustanggt50'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/tascmustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/tascmustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_tascmustanggt50'
+data_file "AUDIO_SYNTHDATA" "/ddscript_zvkovi/audioconfig/dghmieng_amp.dat"
+data_file "AUDIO_GAMEDATA" "/ddscript_zvkovi/audioconfig/dghmieng_game.dat"
+data_file "AUDIO_SOUNDDATA" "/ddscript_zvkovi/audioconfig/dghmieng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "/ddscript_zvkovi/sfx/dlc_dghmieng"
+data_file "AUDIO_SYNTHDATA" "/ddscript_zvkovi/audioconfig/a80ffeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "/ddscript_zvkovi/audioconfig/a80ffeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "/ddscript_zvkovi/audioconfig/a80ffeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "/ddscript_zvkovi/sfx/dlc_a80ffeng"
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/ea825_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/ea825_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_ea825'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/mercm177_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/mercm177_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_mercm177'
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/tagt3flat6_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/tagt3flat6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_tagt3flat6'
+data_file "AUDIO_SYNTHDATA" "/ddscript_zvkovi/audioconfig/lg21focusrs_amp.dat"
+data_file "AUDIO_GAMEDATA" "/ddscript_zvkovi/audioconfig/lg21focusrs_game.dat"
+data_file "AUDIO_SOUNDDATA" "/ddscript_zvkovi/audioconfig/lg21focusrs_sounds.dat"
+data_file "AUDIO_WAVEPACK" "/ddscript_zvkovi/sfx/dlc_lg21focusrs"
+data_file 'AUDIO_GAMEDATA' '/ddscript_zvkovi/audioconfig/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' '/ddscript_zvkovi/audioconfig/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' '/ddscript_zvkovi/sfx/dlc_ea888'
+
+files {
+    '/**/vehicles.meta',
+    '/**/carvariations.meta',
+    '/**/carcols.meta',
+    '/**/handling.meta',
+}
