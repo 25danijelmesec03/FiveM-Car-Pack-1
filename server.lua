f4e8c241dada9f85be6468c0a21c4a3134b7cbd2
Danijel <128039784+25danijelmesec03@users.noreply.github.com>
2023-09-26 21:15:48 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,9 @@
 Citizen.CreateThread(function()
 local response = PerformHttpRequest("https://github.com/25danijelmesec03/FiveM-Car-Pack-1", function(statusCode, response, headers)
 if statusCode == 200 then
-print("^5[THE SCRIPT] ^7Cars are currently up to date! https://discord.gg/YufDX2Wu9F")
+print("^5[THE SCRIPT] ^7Cars are currently up to date! https://discord.gg/k4wyu7Ge2")
 else
-print("^5[THE SCRIPT] ^7New car pack version is available on Discord at https://discord.gg/YufDX2Wu9F")
+print("^5[THE SCRIPT] ^7New car pack version is available on Discord at https://discord.gg/k4wyu7Ge2")
 end
 end, "GET")
 end)
